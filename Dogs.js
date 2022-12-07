113f6eea2a79e29551ac90cc0e3c5f24cbc1199f
Sanduni Senarath <sandunisenarath@gmail.com>
2022-12-07 01:22:07 -0500

    functionality added to like and dislike buttons

--- Dogs.js
+++ Dogs.js
@@ -1,6 +1,31 @@
-
-class Dog{
-    constructor(data){
+class Dog {
+    constructor(data) {
         Object.assign(this,data)
     }
+
+    getDogProfileHtml(){
+    const{name, avatar,age,bio, hasBeenSwiped,hasBeenLiked} = this
+    return `
+    <img class ="dog-pictures" src="${avatar}" alt="cute dog">
+    <p id = "bio"><span class ="bold">${name}, ${age}</span><br>${bio}</p>`
+    }
+
+    // getCharacterHtml() {
+    //     const { name, avatar, health, diceArray} = this
+    //     const healthBar = this.getHealthBarHtml()
+        
+
+    //     return `<div class="character-card">
+    //         <h4 class="name"> ${name} </h4>
+    //         <img class="avatar" src="${avatar}" />
+    //         <div class="health">health: <b> ${health} </b></div>
+    //         ${healthBar}
+    //         <div class="dice-container">    
+    //             ${diceArray}
+    //         </div>
+    //     </div>`
+
+    // }
 }
+
+export default Dog
--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-document.getElementById("dislike-btn").addEventListener('click',() => console.log(`clicked`))
+import dogs from "./data.js"
+import Dog from "./Dogs.js"
+
+document.getElementById('dislike-btn').addEventListener('click', dislike)
+document.getElementById('like-btn').addEventListener('click', like)
+// make "like" and "nope" badge come up when dislike and like are clicked
+
+
+let dogCount = -1
+function getNewDog() {
+    if (dogCount <= dogs.length) {
+        dogCount++
+        return new Dog(dogs[dogCount])
+    } else {
+        return {}
+    }
+}
+
+let dog1 = getNewDog()
+console.log(dog1)
+
+function render() {
+    document.getElementById("badge").innerHTML = ``
+    document.getElementById("profile-card").innerHTML = dog1.getDogProfileHtml()
+}
+
+render()
+
+function dislike() {
+    document.getElementById("badge").innerHTML = `<img src="images/badge-nope.png" alt ="like-badge">`
+    setTimeout(() => {
+        dog1 = getNewDog()
+        render()
+    }, 2000)
+}
+
+function like() {
+    document.getElementById("badge").innerHTML = `<img src="images/badge-like.png" alt ="like-badge">`
+    setTimeout(() => {
+        dog1 = getNewDog()
+        render()
+    }, 2000)
+}
+
+
+
+
+
+
+
+
